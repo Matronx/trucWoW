048da4451a07600bdecc1792a381ab355e509a18
Matronx <mathiasolejnik@hotmail.com>
2017-01-04 19:35:06 -0300

    test #2
    #Testing

--- trucWoW.lua
+++ trucWoW.lua
@@ -2,10 +2,10 @@
 --	SetPortraitToTexture(self.portrait, "Interface\\Icons\\INV_Misc_EngGizmos_30")
 --end
 
-function ElTruco_OnLoad(self)
-	--print("Mostrando");
-	--self:Show();
-	--print("Escondido");
+function trucWoW_OnLoad(self)
+	print("Mostrando");
+	self:Show();
+	print("Escondido");
 end
 
 
@@ -16,11 +16,11 @@ local function SayHello	(self)
 	self:Hide();
 end
 -- Crear Frame
-local MyFrame = CreateFrame("button" , "MyFrame" , UIParent , "UIPanelButtonTemplate");
+local MyFrame = CreateFrame("Frame" , "MyFrame" , UIParent );
 
-MyFrame:SetPoint("CENTER" , 0 ,0);
-MyFrame:SetWidth(800);
-MyFrame:SetHeight(22);
-MyFrame:SetScript("OnClick"  , SayHello);
-MyFrame:SetScript("OnEnter"  , print("Mounse Enter"));
-MyFrame:SetScript("OnLeave"  , print("Mounse Leave"));
+--MyFrame:SetPoint("CENTER" , 0 ,0);
+--MyFrame:SetWidth(800);
+--MyFrame:SetHeight(22);
+--MyFrame:SetScript("OnClick"  , SayHello);
+--MyFrame:SetScript("OnEnter"  , print("Mounse Enter"));
+--MyFrame:SetScript("OnLeave"  , print("Mounse Leave"));
