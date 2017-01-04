35d76e20d6c82d49635dc3138ff0865c92eac702
Matronx <mathiasolejnik@hotmail.com>
2017-01-04 18:54:02 -0300

    Logica del Addon

---
+++ trucWoW.lua
@@ -0,0 +1,26 @@
+--function BagBuddy_OnLoad(self)
+--	SetPortraitToTexture(self.portrait, "Interface\\Icons\\INV_Misc_EngGizmos_30")
+--end
+
+function ElTruco_OnLoad(self)
+	--print("Mostrando");
+	--self:Show();
+	--print("Escondido");
+end
+
+
+
+local function SayHello	(self)
+	-- body
+	print("Cerrando");
+	self:Hide();
+end
+-- Crear Frame
+local MyFrame = CreateFrame("button" , "MyFrame" , UIParent , "UIPanelButtonTemplate");
+
+MyFrame:SetPoint("CENTER" , 0 ,0);
+MyFrame:SetWidth(800);
+MyFrame:SetHeight(22);
+MyFrame:SetScript("OnClick"  , SayHello);
+MyFrame:SetScript("OnEnter"  , print("Mounse Enter"));
+MyFrame:SetScript("OnLeave"  , print("Mounse Leave"));
